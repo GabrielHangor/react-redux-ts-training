6a203ff445b367e0a7d91a84946197492d961d02
Gabriel <gaba93@yandex.ru>
2021-11-26 16:28:29 +0300

    useActions hook to combine action creators

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -2,13 +2,14 @@ import React, { useEffect } from 'react';
 import { useTypedSelector } from './../hooks/useTypedSelector';
 import { useDispatch } from 'react-redux';
 import { fetchUsers } from './../store/action-creators/user';
+import { useActions } from '../hooks/useActions';
 
 const UserList: React.FC = () => {
   const { users, loading, error } = useTypedSelector((state) => state.user);
-  const dispatch = useDispatch();
+  const { fetchUsers } = useActions();
 
   useEffect(() => {
-    dispatch(fetchUsers());
+    fetchUsers();
   }, []);
 
   if (loading) {
